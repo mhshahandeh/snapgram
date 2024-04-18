f366363cb7ae152e191104494c6ef346b1bafba6
mhshahandeh <shahandeh30@gmail.com>
2024-04-18 09:01:47 +0330

    setup file & folder structure & routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello Snapgram!</h1>;
+  return (
+    <Routes>
+      {/* public routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/sing-in" element={<SigninForm />} />
+        <Route path="/sing-up" element={<SignupForm />} />
+      </Route>
+
+      {/* private routes */}
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
